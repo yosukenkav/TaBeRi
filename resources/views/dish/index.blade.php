eb430394d64b30b833f8b78eff5bb6b7aea3af2d
takuro913 <atkgdsfhsk.jp@gmail.com>
2023-12-16 17:57:38 +0900

    登録、一覧画面

---
+++ resources/views/dish/index.blade.php
@@ -0,0 +1,42 @@
+<!-- resources/views/dish/index.blade.php -->
+
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-200">
+      {{ __('一覧') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white dark:bg-gray-800 border-b border-grey-200 dark:border-gray-800">
+          <table class="text-center w-full border-collapse">
+            <thead>
+              <tr>
+                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">体重(kg)</th>
+                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">プロテイン(杯数)</th>
+
+              </tr>
+            </thead>
+            <tbody> <!--情報一覧-->
+              @foreach ($dishes as $dish)
+              <tr class="hover:bg-gray-lighter">
+                    <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600 dark:text-gray-200" white-space: nowrap>{{$dish->weight}}</td>
+                    <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600 dark:text-gray-200" white-space: nowrap>{{$dish->protein_drinks}}</td>
+                  
+                  <div class="flex">
+                    <!--更新ボタン-->
+                    <!--削除ボタン-->
+                  </div>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
+
