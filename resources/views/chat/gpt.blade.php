bc60e798f2003ffe511cef3594b30e0303748908
takuro913 <atkgdsfhsk.jp@gmail.com>
2023-12-28 16:59:02 +0900

    Chatgpt 実装準備、お試し

---
+++ resources/views/chat/gpt.blade.php
@@ -0,0 +1,45 @@
+<!-- resources/views/chat/index.blade.php -->
+
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-200">
+      {{ __('chat Index') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white dark:bg-gray-800 border-b border-grey-200 dark:border-gray-800">
+          <table class="text-center w-full border-collapse">
+            <thead>
+              <tr>
+                <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">chat</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+              <tr class="hover:bg-gray-lighter">
+                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
+                 
+                  <div class="flex">
+                    <form method="POST">
+                        @csrf
+                        <textarea rows="10" cols="50" name="sentence">{{ isset($sentence) ? $sentence : '' }}</textarea>
+                        <button type="submit">ChatGPT</button>
+                    </form>
+
+                    {{-- Œ‹‰Ê --}}
+                    {{ isset($chat_response) ? $chat_response : '' }}
+                  </div>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
+
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,6 +28,11 @@
             {{ __('登録') }}
           </x-nav-link>
         </div>
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+          <x-nav-link :href="route('chat.index')" :active="request()->routeIs('chat.index')">
+            {{ __('Chat') }}
+          </x-nav-link>
+        </div>
       </div>
 
       <!-- Settings Dropdown -->
@@ -94,6 +99,12 @@
         {{ __('登録') }}
       </x-responsive-nav-link>
     </div>
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('chat.index')" :active="request()->routeIs('chat.index')">
+        {{ __('Chat') }}
+      </x-responsive-nav-link>
+    </div>
+   
 
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
